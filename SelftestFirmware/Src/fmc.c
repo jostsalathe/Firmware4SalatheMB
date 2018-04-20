7712ade6a3895fba754244dea7509ca7fcfa3748
jostsalathe <jostsalathe@gmail.com>
2018-04-20 21:33:16 +0200

    tightened the SDRAM timings according to the data sheet

--- SelftestFirmware/Src/fmc.c
+++ SelftestFirmware/Src/fmc.c
@@ -78,12 +78,12 @@ void MX_FMC_Init(void)
   hsdram1.Init.ReadPipeDelay = FMC_SDRAM_RPIPE_DELAY_0;
   /* SdramTiming */
   SdramTiming.LoadToActiveDelay = 2;
-  SdramTiming.ExitSelfRefreshDelay = 10;
-  SdramTiming.SelfRefreshTime = 6;
-  SdramTiming.RowCycleDelay = 10;
-  SdramTiming.WriteRecoveryTime = 4;
-  SdramTiming.RPDelay = 3;
-  SdramTiming.RCDDelay = 3;
+  SdramTiming.ExitSelfRefreshDelay = 7;
+  SdramTiming.SelfRefreshTime = 5;
+  SdramTiming.RowCycleDelay = 7;
+  SdramTiming.WriteRecoveryTime = 3;
+  SdramTiming.RPDelay = 2;
+  SdramTiming.RCDDelay = 2;
 
   if (HAL_SDRAM_Init(&hsdram1, &SdramTiming) != HAL_OK)
   {
