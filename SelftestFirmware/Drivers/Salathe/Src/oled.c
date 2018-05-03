4bf96012c9b782e24731b0f53cc735413ea4ef6e
jostsalathe <jostsalathe@gmail.com>
2018-05-03 11:42:57 +0200

    updated oled driver to be way more efficient by using more of the graphic acceleration commands of the SSD1331 oled controller

--- SelftestFirmware/Drivers/Salathe/Src/oled.c
+++ SelftestFirmware/Drivers/Salathe/Src/oled.c
@@ -20,48 +20,27 @@
 #include <stdlib.h>
 
 //defines
-#define OLED_DC_PIN   SPI1_DC_OLED_Pin
-#define OLED_DC_PORT  SPI1_DC_OLED_GPIO_Port
-#define OLED_CS_PIN   SPI1_CS_OLED_Pin
-#define OLED_CS_PORT  SPI1_CS_OLED_GPIO_Port
+#define OLED_DC_PIN		SPI1_DC_OLED_Pin
+#define OLED_DC_PORT	SPI1_DC_OLED_GPIO_Port
+#define OLED_CS_PIN		SPI1_CS_OLED_Pin
+#define OLED_CS_PORT	SPI1_CS_OLED_GPIO_Port
 
-#define SSD1331_CMD_DRAWLINE 		0x21
-#define SSD1331_CMD_DRAWRECT 		0x22
-#define SSD1331_CMD_COPY	 		0x23
-#define SSD1331_CMD_CLEAR_WINDOW	0x25
-#define SSD1331_CMD_FILL 			0x26
-#define SSD1331_CMD_SETCOLUMN 		0x15
-#define SSD1331_CMD_SETROW    		0x75
-#define SSD1331_CMD_CONTRASTA 		0x81
-#define SSD1331_CMD_CONTRASTB 		0x82
-#define SSD1331_CMD_CONTRASTC		0x83
-#define SSD1331_CMD_MASTERCURRENT 	0x87
-#define SSD1331_CMD_SETREMAP 		0xA0
-#define SSD1331_CMD_STARTLINE 		0xA1
-#define SSD1331_CMD_DISPLAYOFFSET 	0xA2
-#define SSD1331_CMD_NORMALDISPLAY 	0xA4
-#define SSD1331_CMD_DISPLAYALLON  	0xA5
-#define SSD1331_CMD_DISPLAYALLOFF 	0xA6
-#define SSD1331_CMD_INVERTDISPLAY 	0xA7
-#define SSD1331_CMD_SETMULTIPLEX  	0xA8
-#define SSD1331_CMD_SETMASTER 		0xAD
-#define SSD1331_CMD_DISPLAYOFF 		0xAE
-#define SSD1331_CMD_DISPLAYON     	0xAF
-#define SSD1331_CMD_POWERMODE 		0xB0
-#define SSD1331_CMD_PRECHARGE 		0xB1
-#define SSD1331_CMD_CLOCKDIV 		0xB3
-#define SSD1331_CMD_PRECHARGEA 		0x8A
-#define SSD1331_CMD_PRECHARGEB 		0x8B
-#define SSD1331_CMD_PRECHARGEC 		0x8C
-#define SSD1331_CMD_PRECHARGELEVEL 	0xBB
-#define SSD1331_CMD_VCOMH 			0xBE
-#define SSD1331_WRITE_DC(x) {HAL_GPIO_WritePin(OLED_DC_PORT, OLED_DC_PIN, x?GPIO_PIN_SET:GPIO_PIN_RESET);}
-#define SSD1331_WRITE_CS(x) {HAL_GPIO_WritePin(OLED_CS_PORT, OLED_CS_PIN, x?GPIO_PIN_SET:GPIO_PIN_RESET);}
+#define SSD1331_WRITE_DC(x)		{HAL_GPIO_WritePin(OLED_DC_PORT, OLED_DC_PIN, x?GPIO_PIN_SET:GPIO_PIN_RESET);}
+#define SSD1331_WRITE_CS(x)		{HAL_GPIO_WritePin(OLED_CS_PORT, OLED_CS_PIN, x?GPIO_PIN_SET:GPIO_PIN_RESET);}
 
-#define OLED_CHAR_WIDTH 5
-#define OLED_CHAR_HEIGHT 8
-#define OLED_HSPACE 1
-#define OLED_VSPACE 2
+#define OLED_IS_COL(x)			(x>=0 && x<OLED_WIDTH)
+#define OLED_IS_ROW(y)			(y>=0 && y<OLED_HEIGHT)
+#define OLED_ASSERT_COL(x)		if(!OLED_IS_COL(x)) return
+#define OLED_ASSERT_ROW(y)		if(!OLED_IS_ROW(y)) return
+
+#define OLED_GET_RED(c)			((c>>11)&0x1F)
+#define OLED_GET_GREEN(c)		((c>>5)&0x3F)
+#define OLED_GET_BLUE(c)		((c)&0x1F)
+
+#define OLED_CHAR_WIDTH		5
+#define OLED_CHAR_HEIGHT	8
+#define OLED_HSPACE			1
+#define OLED_VSPACE			2
 
 //variables
 SPI_HandleTypeDef *hspiOled;
@@ -87,17 +66,6 @@ void oledCurSet(uint8_t x, uint8_t y){
 
 void oledCurHome(){
 	oledCurSet(0,0);
-}
-
-void oledClear(){
-    oledWriteCmd(SSD1331_CMD_CLEAR_WINDOW);	// 0x25
-    oledWriteCmd(0);						// Column 1
-    oledWriteCmd(0);						// Row 1
-    oledWriteCmd(OLED_WIDTH-1);				// Column 2
-    oledWriteCmd(OLED_HEIGHT-1);			// Row 2
-    oledCurX = 0;
-    oledCurY = 0;
-    vTaskDelay(10);
 }
 
 void oledPushPixels(uint16_t *color, uint16_t n){
@@ -141,21 +109,9 @@ void oledPutChar(char c, uint16_t color){
 		oledCurX = 0;
 		if(oledCurY+OLED_CHAR_HEIGHT+OLED_VSPACE>OLED_HEIGHT){		//reached bottom of display
 			uint8_t shiftAmount = oledCurY+OLED_CHAR_HEIGHT+OLED_VSPACE-OLED_HEIGHT;
-			oledWriteCmd(SSD1331_CMD_COPY);				// 0x23
-			oledWriteCmd(0);							// Column start
-			oledWriteCmd(shiftAmount);					// Row start
-			oledWriteCmd(OLED_WIDTH-1);					// Column end
-			oledWriteCmd(OLED_HEIGHT-1);				// Row end
-			oledWriteCmd(0);							// Column target
-			oledWriteCmd(0);							// Row target
+			oledCopyWindow(0, shiftAmount, OLED_WIDTH, OLED_HEIGHT-shiftAmount, 0, 0);
 			oledCurY -= shiftAmount;
-			vTaskDelay(10);
-			oledWriteCmd(SSD1331_CMD_CLEAR_WINDOW);		// 0x25
-			oledWriteCmd(0);							// Column 1
-			oledWriteCmd(oledCurY-OLED_VSPACE);			// Row 1
-			oledWriteCmd(OLED_WIDTH-1);					// Column 2
-			oledWriteCmd(OLED_HEIGHT-1);				// Row 2
-			vTaskDelay(10);
+			oledClearWindow(0, oledCurY-OLED_VSPACE, OLED_WIDTH, shiftAmount);
 		}
 	}
 
@@ -163,25 +119,23 @@ void oledPutChar(char c, uint16_t color){
 		oledFillRectangel(oledCurX, oledCurY, oledCurX+OLED_CHAR_WIDTH-1+OLED_HSPACE, oledCurY+OLED_CHAR_HEIGHT-1+OLED_VSPACE, 0);
 		oledCurX += OLED_CHAR_WIDTH+OLED_HSPACE;
 	}else if(c!='\n' && c!='\r'){
-		oledSetWindow(oledCurX, oledCurY, oledCurX+OLED_CHAR_WIDTH-1+OLED_HSPACE, oledCurY+OLED_CHAR_HEIGHT-1+OLED_VSPACE);
+		oledClearWindow(oledCurX+OLED_CHAR_WIDTH, oledCurY, OLED_HSPACE, OLED_CHAR_HEIGHT);
+		oledClearWindow(oledCurX, oledCurY+OLED_CHAR_HEIGHT, OLED_CHAR_WIDTH+OLED_HSPACE, OLED_VSPACE);
+		oledSetWindow(oledCurX, oledCurY, OLED_CHAR_WIDTH, OLED_CHAR_HEIGHT);
 		const uint8_t *charCol = oledCharMap[(uint8_t) c];
 		uint8_t row=0, col=0;
 		uint8_t rowMask = 0x80;
-		uint16_t charMatrix[(OLED_CHAR_HEIGHT+OLED_VSPACE)*(OLED_CHAR_WIDTH+OLED_HSPACE)], i=0;
-		for(row=0; row<OLED_CHAR_HEIGHT+OLED_VSPACE; ++row){
-			for(col=0; col<OLED_CHAR_WIDTH+OLED_HSPACE; ++col){
-				if(col<OLED_CHAR_WIDTH && row<OLED_CHAR_HEIGHT){
-					charMatrix[i] = (charCol[col]&rowMask)?color:0;
-				}else{
-					charMatrix[i] = 0;
-				}
+		uint16_t charMatrix[(OLED_CHAR_HEIGHT)*(OLED_CHAR_WIDTH)], i=0;
+		for(row=0; row<OLED_CHAR_HEIGHT; ++row){
+			for(col=0; col<OLED_CHAR_WIDTH; ++col){
+				charMatrix[i] = (charCol[col]&rowMask)?color:0;
 				++i;
 			}
 			rowMask >>= 1;
 		}
 		oledPushPixels(charMatrix,i);
 		oledCurX += OLED_CHAR_WIDTH+OLED_HSPACE;
-		oledSetWindow(0, 0, OLED_WIDTH-1, OLED_HEIGHT-1);
+		oledSetWindow(0, 0, OLED_WIDTH, OLED_HEIGHT);
 	}
 }
 
@@ -198,76 +152,142 @@ void oledSetup(SPI_HandleTypeDef* hspi) {
 	oledCurX = 0;
 	oledCurY = 0;
 	//initialization sequence
-    oledWriteCmd(SSD1331_CMD_DISPLAYOFF);  	// 0xAE
-    oledWriteCmd(SSD1331_CMD_SETREMAP); 	// 0xA0
+	oledWriteCmd(SSD1331_CMD_NOP);
+    oledWriteCmd(SSD1331_CMD_DISPLAYOFF);
+    oledWriteCmd(SSD1331_CMD_SETREMAP);
     oledWriteCmd(0x72);						// RGB Color
-    oledWriteCmd(SSD1331_CMD_STARTLINE); 	// 0xA1
+    oledWriteCmd(SSD1331_CMD_STARTLINE);
     oledWriteCmd(0x0);
-    oledWriteCmd(SSD1331_CMD_DISPLAYOFFSET);// 0xA2
+    oledWriteCmd(SSD1331_CMD_DISPLAYOFFSET);
     oledWriteCmd(0x0);
-    oledWriteCmd(SSD1331_CMD_NORMALDISPLAY);// 0xA4
-    oledWriteCmd(SSD1331_CMD_SETMULTIPLEX); // 0xA8
-    oledWriteCmd(0x3F);  					// 0x3F 1/64 duty
-    oledWriteCmd(SSD1331_CMD_SETMASTER);  	// 0xAD
+    oledWriteCmd(SSD1331_CMD_NORMALDISPLAY);
+    oledWriteCmd(SSD1331_CMD_SETMULTIPLEX);
+    oledWriteCmd(0x3F);  					// 1/64 duty
+    oledWriteCmd(SSD1331_CMD_SETMASTER);
     oledWriteCmd(0x8E);
-    oledWriteCmd(SSD1331_CMD_POWERMODE);  	// 0xB0
+    oledWriteCmd(SSD1331_CMD_POWERMODE);
     oledWriteCmd(0x0B);
-    oledWriteCmd(SSD1331_CMD_PRECHARGE);  	// 0xB1
+    oledWriteCmd(SSD1331_CMD_PRECHARGE);
     oledWriteCmd(0x31);
-    oledWriteCmd(SSD1331_CMD_CLOCKDIV);  	// 0xB3
+    oledWriteCmd(SSD1331_CMD_CLOCKDIV);
     oledWriteCmd(0xF0);  // 7:4 = Oscillator Frequency, 3:0 = CLK Div Ratio (A[3:0]+1 = 1..16)
-    oledWriteCmd(SSD1331_CMD_PRECHARGEA);  	// 0x8A
+    oledWriteCmd(SSD1331_CMD_PRECHARGEA);
     oledWriteCmd(0x64);
-    oledWriteCmd(SSD1331_CMD_PRECHARGEB);  	// 0x8B
+    oledWriteCmd(SSD1331_CMD_PRECHARGEB);
     oledWriteCmd(0x78);
-    oledWriteCmd(SSD1331_CMD_PRECHARGEC);  	// 0x8C
+    oledWriteCmd(SSD1331_CMD_PRECHARGEC);
     oledWriteCmd(0x64);
-    oledWriteCmd(SSD1331_CMD_PRECHARGELEVEL);// 0xBB
+    oledWriteCmd(SSD1331_CMD_PRECHARGELEVEL);
     oledWriteCmd(0x3A);
-    oledWriteCmd(SSD1331_CMD_VCOMH);  		// 0xBE
+    oledWriteCmd(SSD1331_CMD_VCOMH);
     oledWriteCmd(0x3E);
-    oledWriteCmd(SSD1331_CMD_MASTERCURRENT);// 0x87
+    oledWriteCmd(SSD1331_CMD_MASTERCURRENT);
     oledWriteCmd(0x06);
-    oledWriteCmd(SSD1331_CMD_CONTRASTA);  	// 0x81
+    oledWriteCmd(SSD1331_CMD_CONTRASTA);
     oledWriteCmd(0x91);
-    oledWriteCmd(SSD1331_CMD_CONTRASTB);  	// 0x82
+    oledWriteCmd(SSD1331_CMD_CONTRASTB);
     oledWriteCmd(0x50);
-    oledWriteCmd(SSD1331_CMD_CONTRASTC);  	// 0x83
+    oledWriteCmd(SSD1331_CMD_CONTRASTC);
     oledWriteCmd(0x7D);
-    oledWriteCmd(SSD1331_CMD_CLEAR_WINDOW);	// 0x25
+    oledWriteCmd(SSD1331_CMD_RECT_FILL);
+    oledWriteCmd(0x1);
+    oledWriteCmd(SSD1331_CMD_CLEAR_WINDOW);
     oledWriteCmd(0);						// Column 1
     oledWriteCmd(0);						// Row 1
     oledWriteCmd(OLED_WIDTH-1);				// Column 2
     oledWriteCmd(OLED_HEIGHT-1);			// Row 2
-    oledWriteCmd(SSD1331_CMD_DISPLAYON);	//--turn on oled panel
+    oledWriteCmd(SSD1331_CMD_DISPLAYON);
 
 }
 
-void oledSetWindow(uint8_t xMin, uint8_t yMin, uint8_t xMax, uint8_t yMax){
+void oledSetWindow(uint8_t posX, uint8_t posY, uint8_t sizeX, uint8_t sizeY) {
+	OLED_ASSERT_COL(sizeX-1);
+	OLED_ASSERT_ROW(sizeY-1);
+	OLED_ASSERT_COL(posX);
+	OLED_ASSERT_ROW(posY);
+	OLED_ASSERT_COL(posX+sizeX-1);
+	OLED_ASSERT_ROW(posY+sizeY-1);
 	oledWriteCmd(SSD1331_CMD_SETCOLUMN);
-    oledWriteCmd(xMin);										// Column start
-    oledWriteCmd(xMax);	// Column end
+    oledWriteCmd(posX);						// Column start
+    oledWriteCmd(posX+sizeX-1);				// Column end
 	oledWriteCmd(SSD1331_CMD_SETROW);
-    oledWriteCmd(yMin);										// Row start
-    oledWriteCmd(yMax);	// Row end
+    oledWriteCmd(posY);						// Row start
+    oledWriteCmd(posY+sizeY-1);				// Row end
 }
 
-void oledFillRectangel(uint8_t xMin, uint8_t yMin, uint8_t xMax, uint8_t yMax, uint16_t color) {
-	oledSetWindow(xMin, yMin, xMax, yMax);
-	oledPushPixeln(color, (xMax-xMin+1) * (yMax-yMin+1));
-	oledSetWindow(0, 0, OLED_WIDTH-1, OLED_HEIGHT-1);
+void oledClearWindow(uint8_t posX, uint8_t posY, uint8_t sizeX, uint8_t sizeY) {
+	OLED_ASSERT_COL(sizeX-1);
+	OLED_ASSERT_ROW(sizeY-1);
+	OLED_ASSERT_COL(posX);
+	OLED_ASSERT_ROW(posY);
+	OLED_ASSERT_COL(posX+sizeX-1);
+	OLED_ASSERT_ROW(posY+sizeY-1);
+    oledWriteCmd(SSD1331_CMD_CLEAR_WINDOW);	// 0x25
+    oledWriteCmd(posX);						// Column 1
+    oledWriteCmd(posY);						// Row 1
+    oledWriteCmd(posX+sizeX-1);				// Column 2
+    oledWriteCmd(posY+sizeY-1);				// Row 2
+	vTaskDelay(1);
+}
+
+void oledClear() {
+	oledClearWindow(0, 0, OLED_WIDTH, OLED_HEIGHT);
+}
+
+void oledCopyWindow(uint8_t sourceX, uint8_t sourceY, uint8_t sizeX, uint8_t sizeY, uint8_t targetX, uint8_t targetY) {
+	OLED_ASSERT_COL(sizeX-1);
+	OLED_ASSERT_ROW(sizeY-1);
+	OLED_ASSERT_COL(sourceX);
+	OLED_ASSERT_ROW(sourceY);
+	OLED_ASSERT_COL(targetX);
+	OLED_ASSERT_ROW(targetY);
+	OLED_ASSERT_COL(sourceX+sizeX-1);
+	OLED_ASSERT_ROW(sourceY+sizeY-1);
+	oledWriteCmd(SSD1331_CMD_COPY);			// 0x23
+	oledWriteCmd(sourceX);					// Column start
+	oledWriteCmd(sourceY);					// Row start
+	oledWriteCmd(sourceX+sizeX-1);			// Column end
+	oledWriteCmd(sourceY+sizeY-1);			// Row end
+	oledWriteCmd(targetX);					// Column target
+	oledWriteCmd(targetY);					// Row target
+	vTaskDelay(1);
+}
+
+void oledDrawRectangel(uint8_t posX, uint8_t posY, uint8_t sizeX, uint8_t sizeY, uint16_t cBorder, uint16_t cFill) {
+	OLED_ASSERT_COL(sizeX-1);
+	OLED_ASSERT_ROW(sizeY-1);
+	OLED_ASSERT_COL(posX);
+	OLED_ASSERT_ROW(posY);
+	OLED_ASSERT_COL(posX+sizeX-1);
+	OLED_ASSERT_ROW(posY+sizeY-1);
+	oledWriteCmd(SSD1331_CMD_DRAWRECT);
+	oledWriteCmd(posX);						// Column 1
+	oledWriteCmd(posY);						// Row 1
+	oledWriteCmd(posX+sizeX-1);				// Column 2
+	oledWriteCmd(posY+sizeY-1);				// Row 2
+	oledWriteCmd(OLED_GET_RED(cBorder)<<1);	// border color red
+	oledWriteCmd(OLED_GET_GREEN(cBorder));	// border color green
+	oledWriteCmd(OLED_GET_BLUE(cBorder)<<1);// border color blue
+	oledWriteCmd(OLED_GET_RED(cFill)<<1);	// fill color red
+	oledWriteCmd(OLED_GET_GREEN(cFill));	// fill color green
+	oledWriteCmd(OLED_GET_BLUE(cFill)<<1);	// fill color blue
+	vTaskDelay(1);
+}
+
+void oledFillRectangel(uint8_t posX, uint8_t posY, uint8_t sizeX, uint8_t sizeY, uint16_t color) {
+	oledDrawRectangel(posX, posY, sizeX, sizeY, color, color);
+}
+
+void oledFillScreen(uint16_t color) {
+	oledFillRectangel(0, 0, OLED_WIDTH, OLED_HEIGHT, color);
 }
 
 void oledProgress(float progress, uint16_t color) {
 	uint8_t progr = progress*(OLED_WIDTH)+0.5;
 	if (progr<OLED_WIDTH)
-		oledFillRectangel(progr, OLED_HEIGHT-1, OLED_WIDTH-1, OLED_HEIGHT-1, 0);
+		oledFillRectangel(progr, OLED_HEIGHT-1, OLED_WIDTH-progr, 1, 0);
 	if (progr)
-		oledFillRectangel(0, OLED_HEIGHT-1, progr-1, OLED_HEIGHT-1, color);
-}
-
-void oledFillScreen(uint16_t color) {
-	oledFillRectangel(0, 0, OLED_WIDTH-1, OLED_HEIGHT-1, color);
+		oledFillRectangel(0, OLED_HEIGHT-1, progr, 1, color);
 }
 
 void oledWriteFontset(uint16_t color){
